892138caf5199ca37a52b4e606ecebc85d5df2f4
Kalydy Barnabás <barnabas.kalydy@gmail.com>
2023-08-28 20:04:25 +0200

    Class work

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  useEffect(() => {
-    fetch(
-      "https://archive-api.open-meteo.com/v1/archive?latitude=48.1031&longitude=20.7781&start_date=2023-06-30&end_date=2023-07-14&hourly=temperature_2m"
-    )
-      .then((data) => data.json())
-      .then((newData) => console.log(newData))
-      .catch((err) => console.log(err));
-  }, []);
+  const [todos, setTodos] = useState(["todo1", "todo2"]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const saveNewTodo = () => {
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (todoToRemove) => {
+    const newTodos = todos.filter((todo) => todo !== todoToRemove);
+    setTodos(newTodos);
+  };
 
   return (
-    <>
-      {/* {countries.map((country) => (
-        <div key={country.name} style={{ margin: "50px" }}>
-          <div>{country.name}</div>
-          <div>{country.capital}</div>
-          <div>{country.population}</div>
-        </div>
-      ))} */}
-    </>
+    <div style={{ fontSize: "30px" }}>
+      {todos.map((todo, index) => {
+        return (
+          <div key={todo}>
+            <input
+              type="checkbox"
+              name={todo}
+              onChange={() => removeTodo(todo)}
+            />
+            <label htmlFor={todo}>
+              {index + 1}. {todo}
+            </label>
+            <br />
+          </div>
+        );
+      })}
+      <div>
+        <input onChange={(e) => setNewTodo(e.target.value)} />
+        <button onClick={saveNewTodo}>Save todo</button>
+      </div>
+    </div>
   );
 }
 
